9af74c8f05334f965f79e95248210d7ced8b637c
Crimsey <kubsok2@wp.pl>
2021-12-10 20:19:11 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+constructor(props){
+super(props);
+
+this.state = {
+  num : '',
+  x : ''
 }
 
+this.updateInput = this.updateInput.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+
+updateInput(event){
+  this.setState({num : event.target.value})
+}
+
+
+handleSubmit(){
+console.log('Your input value is: ' + this.state.num)
+let n1 = 0, n2 = 1, nextTerm;
+
+for (let i = 1; i <= this.state.num; i++) {
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+this.setState({x : n1})
+
+}
+
+  render() {
+    return (
+      <div className="App">
+      <p>Podaj wartość cakowita K aby obliczyc K-ty element: </p>
+       <input type="text" onChange={this.updateInput}></input>
+       <input value="Oblicz" type="submit" onClick={this.handleSubmit} ></input>
+       <p>K-ty element: {this.state.x}</p>
+    <img src={logo} className="App-logo" alt="logo" />
+
+      </div>
+
+    );
+  }
+}
 export default App;
